72266e69f43877d6298fc10330b5425d22930785
Roy Mosby <ego.madking@gmail.com>
2020-12-08 21:31:28 -0500

    commit tree f551846d27893f131d69d0e77fc9d89ca12356d0

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |attendee|
+    badges << badge_maker(attendee)
+  end
+  badges
+end
+
+def assign_rooms(attendees)
+  assignments = []
+  attendees.each do |attendee|
+    assignments << "Hello, #{attendee}! You'll be assigned to room #{attendees.index(attendee) + 1}!"
+  end
+  assignments
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
